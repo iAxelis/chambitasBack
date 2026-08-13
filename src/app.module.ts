1b2bbfa8293f32f958325531427da3a453ec5248
iAxelis <elaxelis701@gmail.com>
2026-08-12 23:56:01 -0600

    feat: crear estructura inicial de recursos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { JobsModule } from './jobs/jobs.module';
+import { ApplicationsModule } from './applications/applications.module';
+import { MatchesModule } from './matches/matches.module';
+import { ContractsModule } from './contracts/contracts.module';
+import { ReviewsModule } from './reviews/reviews.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, JobsModule, ApplicationsModule, MatchesModule, ContractsModule, ReviewsModule],
   controllers: [AppController],
   providers: [AppService],
 })
