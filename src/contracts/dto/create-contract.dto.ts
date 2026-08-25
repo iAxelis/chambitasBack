e1c993fb2f3709d9932330631b309ae8a4903690
YairB <sinc@gmail.com>
2026-08-25 15:25:12 -0600

    feat(validaciones): agregue validaciones a DTOs y jobskill

--- src/contracts/dto/create-contract.dto.ts
+++ src/contracts/dto/create-contract.dto.ts
@@ -1,18 +1,21 @@
-import {
-  IsInt,
-  IsNotEmpty,
-} from 'class-validator';
+import { IsInt, IsNotEmpty, Min, Max } from 'class-validator';
 
 export class CreateContractDto {
   @IsInt()
   @IsNotEmpty()
+  @Min(1)
+  @Max(2147483647)
   matchId!: number;
 
   @IsInt()
   @IsNotEmpty()
+  @Min(1)
+  @Max(2147483647)
   jobId!: number;
 
   @IsInt()
   @IsNotEmpty()
+  @Min(1)
+  @Max(2147483647)
   userId!: number;
 }
--- src/job-skills/dto/create-job-skill.dto.ts
+++ src/job-skills/dto/create-job-skill.dto.ts
@@ -1,14 +1,15 @@
-import {
-  IsInt,
-  IsNotEmpty,
-} from 'class-validator';
+import { IsInt, IsNotEmpty, Min, Max } from 'class-validator';
 
 export class CreateJobSkillDto {
   @IsInt()
   @IsNotEmpty()
+  @Min(1)
+  @Max(2147483647)
   jobId!: number;
 
   @IsInt()
   @IsNotEmpty()
+  @Min(1)
+  @Max(2147483647)
   skillId!: number;
 }
