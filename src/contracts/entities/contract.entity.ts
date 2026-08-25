a1266499f1c8a6ec0b0910e3bc94fc715a198eef
YairB <sinc@gmail.com>
2026-08-25 15:05:09 -0600

    feat(contract: agregue validaciones class-validator en contract)

--- src/contracts/entities/contract.entity.ts
+++ src/contracts/entities/contract.entity.ts
@@ -3,21 +3,42 @@ import {
   Entity,
   PrimaryGeneratedColumn,
 } from 'typeorm';
+import {
+  IsInt,
+  Min,
+  Max,
+  IsString,
+  MinLength,
+  MaxLength
+} from 'class-validator';
 
 @Entity('contracts')
 export class Contract {
   @PrimaryGeneratedColumn()
   id!: number;
 
+  
   @Column()
+  @IsInt()
+  @Min(1)
+  @Max(2147483647)
   matchId!: number;
 
   @Column()
+  @IsInt()
+  @Min(1)
+  @Max(2147483647)
   jobId!: number;
 
   @Column()
+  @IsInt()
+  @Min(1)
+  @Max(2147483647)
   userId!: number;
 
   @Column({ default: 'ACTIVE' })
+  @IsString()
+  @MinLength(1)
+  @MaxLength(20)
   status!: string;
 }
