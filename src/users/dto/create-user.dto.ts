383eed31a5178e41b14fe13ac3e758270b6fe2f3
YairB <sinc@gmail.com>
2026-08-24 22:14:30 -0600

    feat(users): mejora validación de creación de usuarios

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -4,21 +4,25 @@ import {
   IsNotEmpty,
   IsOptional,
   IsString,
+  MaxLength,
   MinLength,
 } from 'class-validator';
 
 export class CreateUserDto {
   @IsString()
   @IsNotEmpty()
+  @MaxLength(100)
   name!: string;
 
   @IsEmail()
   @IsNotEmpty()
+  @MaxLength(255)
   email!: string;
 
   @IsString()
   @IsNotEmpty()
   @MinLength(6)
+  @MaxLength(100)
   password!: string;
 
   @IsOptional()
